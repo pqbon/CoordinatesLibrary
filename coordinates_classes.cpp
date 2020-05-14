97266f461e34527a8834128e0dca1f3dc8e0c3ea
Will Deutsch <wdeutsch@pqbon.com>
2020-05-14 23:11:38 +0100

    Add support for doubles...

--- coordinates_classes.cpp
+++ coordinates_classes.cpp
@@ -9,6 +9,8 @@
 
 
 using namespace CoordinatesNS;
+using namespace CoordinatesNS::Float;
+
 using namespace std::literals::string_literals;
 using namespace std::literals::string_view_literals;
 
@@ -19,61 +21,61 @@ int main()
 {
     std::cout << "Test code for navigation code"sv << std::endl;
 
-    CoordinatesNS::CoordinatesDF test0DF { 40.7030526_ND, 74.0157877_WD }; //New York Battery Part
-    CoordinatesNS::CoordinatesDF copytest0DF(test0DF);
-    CoordinatesNS::CoordinatesDMF copytest0DMF(test0DF);
-    //CoordinatesNS::CoordinatesDMF copytest0DMF;
+    CoordinatesNS::Float::CoordinatesDF test0DF { 40.7030526_ND, 74.0157877_WD }; //New York Battery Part
+    CoordinatesNS::Float::CoordinatesDF copytest0DF(test0DF);
+    CoordinatesNS::Float::CoordinatesDMF copytest0DMF(test0DF);
+    //CoordinatesNS::Float::CoordinatesDMF copytest0DMF;
     //convert(copytest0DMF, test0DF);
-    CoordinatesNS::CoordinatesDMSF copytest0DMSF(test0DF);
-    //CoordinatesNS::CoordinatesDMSF copytest0DMSF;
+    CoordinatesNS::Float::CoordinatesDMSF copytest0DMSF(test0DF);
+    //CoordinatesNS::Float::CoordinatesDMSF copytest0DMSF;
     //convert(copytest0DMSF, test0DF);
 
-    CoordinatesNS::CoordinatesDMF test1DMF { DM_tF {51.0f, 30.11666666666666666666666666f}, DM_tF {0.0f, 5.366666666666666f} }; //London Gallons Hill 51.5014684 0.0912972
-    CoordinatesNS::CoordinatesDF copytest1DF(test1DMF);
+    CoordinatesNS::Float::CoordinatesDMF test1DMF { DM_tF {51.0f, 30.11666666666666666666666666f}, DM_tF {0.0f, 5.366666666666666f} }; //London Gallons Hill 51.5014684 0.0912972
+    CoordinatesNS::Float::CoordinatesDF copytest1DF(test1DMF);
     //CoordinatesNS::CoordinatesDF copytest1DF;
     //convert(copytest1DF, test1DMF);
-    CoordinatesNS::CoordinatesDMF copytest1DMF(test1DMF);
-    CoordinatesNS::CoordinatesDMSF copytest1DMSF(test1DMF);
+    CoordinatesNS::Float::CoordinatesDMF copytest1DMF(test1DMF);
+    CoordinatesNS::Float::CoordinatesDMSF copytest1DMSF(test1DMF);
     //CoordinatesNS::CoordinatesDMSF copytest1DMSF;
     //convert(copytest1DMSF, test1DMF);
 
-    CoordinatesNS::CoordinatesDMSF test2DMSF { DMS_tF {52.0f, 12.0f, 17.0f}, DMS_tF {0.0f, 7.0f, 2.0f} }; //Cambridge Kings College Catherderal front lawn
-    CoordinatesNS::CoordinatesDF copytest2DF { test2DMSF };
+    CoordinatesNS::Float::CoordinatesDMSF test2DMSF { DMS_tF {52.0f, 12.0f, 17.0f}, DMS_tF {0.0f, 7.0f, 2.0f} }; //Cambridge Kings College Catherderal front lawn
+    CoordinatesNS::Float::CoordinatesDF copytest2DF { test2DMSF };
     //CoordinatesNS::CoordinatesDF copytest2DF;
     //convert(copytest2DF, test2DMSF);
-    CoordinatesNS::CoordinatesDMF copytest2DMF { test2DMSF };
+    CoordinatesNS::Float::CoordinatesDMF copytest2DMF { test2DMSF };
     //CoordinatesNS::CoordinatesDMF copytest2DMF;
     //convert(copytest2DMF, test2DMSF);
-    CoordinatesNS::CoordinatesDMSF copytest2DMSF(test2DMSF);
+    CoordinatesNS::Float::CoordinatesDMSF copytest2DMSF(test2DMSF);
 
-    CoordinatesNS::Cart_vec_tF const testCartVec0 { 10, 10 };
-    CoordinatesNS::Cart_vec_tF const testCartVec1 { 20, 20 };
-    CoordinatesNS::Cart_vec_tF const testCartVecA { testCartVec0 + testCartVec1 };
+    CoordinatesNS::Float::Cart_vec_tF const testCartVec0 { 10, 10 };
+    CoordinatesNS::Float::Cart_vec_tF const testCartVec1 { 20, 20 };
+    CoordinatesNS::Float::Cart_vec_tF const testCartVecA { testCartVec0 + testCartVec1 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVec0 { 20, 45 };
-    CoordinatesNS::Nav_vec_tF const testNavVec1 { 30, 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec0 { 20, 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec1 { 30, 45 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVecA { testNavVec0 + testNavVec1 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVecA { testNavVec0 + testNavVec1 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVec0A { 20, 45 };
-    CoordinatesNS::Nav_vec_tF const testNavVec1A { 30, 180 + 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec0A { 20, 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec1A { 30, 180 + 45 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVecAA { testNavVec0A + testNavVec1A };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVecAA { testNavVec0A + testNavVec1A };
 
-    CoordinatesNS::Nav_vec_tF const testNavVec0B { 30, 0 };
-    CoordinatesNS::Nav_vec_tF const testNavVec1B { 30, 180 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec0B { 30, 0 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec1B { 30, 180 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVecAB { testNavVec0B + testNavVec1B };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVecAB { testNavVec0B + testNavVec1B };
 
-    CoordinatesNS::Nav_vec_tF const testNavVec0C { 20, 90 };
-    CoordinatesNS::Nav_vec_tF const testNavVec1C { 30, 270 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec0C { 20, 90 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec1C { 30, 270 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVecAC { testNavVec0C + testNavVec1C };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVecAC { testNavVec0C + testNavVec1C };
 
-    CoordinatesNS::Nav_vec_tF const testNavVec0D { 30, 90 + 45 };
-    CoordinatesNS::Nav_vec_tF const testNavVec1D { 30, 180 + 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec0D { 30, 90 + 45 };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVec1D { 30, 180 + 45 };
 
-    CoordinatesNS::Nav_vec_tF const testNavVecAD { testNavVec0D + testNavVec1D };
+    CoordinatesNS::Float::Nav_vec_tF const testNavVecAD { testNavVec0D + testNavVec1D };
 
     std::cout << "test0DF "sv << test0DF << " "sv << copytest0DF << " "sv << copytest0DMF << " "sv << copytest0DMSF << " Phi "sv << test0DF.phi() << " Lambda "sv << test0DF.lambda() << std::endl;
     std::cout << "test1DMF "sv << test1DMF << " "sv << copytest1DF << " "sv << copytest1DMF << " "sv << copytest1DMSF << " Phi "sv << test1DMF.phi() << " Lambda "sv << test1DMF.lambda() << std::endl;
@@ -86,13 +88,13 @@ int main()
     std::cout << "testNavVec0D "sv << testNavVec0D << " + testNavVec1D "sv << testNavVec1D << " = testNavVecAD "sv << testNavVecAD << std::endl;
 
     std::cout << "Maths test..." << std::endl << std::endl << std::endl;
-    CoordinatesNS::CoordinatesDMF LAX { DM_tF {33.0f, 57.0f},  DM_tF {-118.0f, 24.0f} };
-    CoordinatesNS::CoordinatesDMF JFK { DM_tF {40.0f, 38.0f}, DM_tF {-73.0f, 47.0f} };
-    CoordinatesNS::CoordinatesDF SFO { D_tF {37.615223f},  D_tF {-122.389977f} };
-    CoordinatesNS::CoordinatesDMSF LHR { DMS_tF {51.0f, 28.0f, 12.0720f}, DMS_tF {-0.0f, 27.0f, 15.4620f} };
-    CoordinatesNS::CoordinatesDMSF PCG { DMS_tF {49.0f, 0.0f, 35.0064f}, DMS_tF {2.0f, 32.0f, 52.0008f} };
-    CoordinatesNS::CoordinatesDF SPBH { D_tF{-36.788433f}, D_tF{174.688766f} };
-    CoordinatesNS::CoordinatesDF JSIA { D_tF{-26.1372399f}, D_tF{28.1975f} };
+    CoordinatesNS::Float::CoordinatesDMF LAX { DM_tF {33.0f, 57.0f},  DM_tF {-118.0f, 24.0f} };
+    CoordinatesNS::Float::CoordinatesDMF JFK { DM_tF {40.0f, 38.0f}, DM_tF {-73.0f, 47.0f} };
+    CoordinatesNS::Float::CoordinatesDF SFO { D_tF {37.615223f},  D_tF {-122.389977f} };
+    CoordinatesNS::Float::CoordinatesDMSF LHR { DMS_tF {51.0f, 28.0f, 12.0720f}, DMS_tF {-0.0f, 27.0f, 15.4620f} };
+    CoordinatesNS::Float::CoordinatesDMSF PCG { DMS_tF {49.0f, 0.0f, 35.0064f}, DMS_tF {2.0f, 32.0f, 52.0008f} };
+    CoordinatesNS::Float::CoordinatesDF SPBH { D_tF{-36.788433f}, D_tF{174.688766f} };
+    CoordinatesNS::Float::CoordinatesDF JSIA { D_tF{-26.1372399f}, D_tF{28.1975f} };
 
     nav_info_dump(LAX, JFK, "LAX"sv, "JFK"sv);
     std::cout << std::endl;
@@ -121,18 +123,18 @@ void nav_info_dump(T0 const& P0, T1 const& P1, std::string_view&& name0, std::st
     auto great_circle { grtcrcl_dist_nm(P0, P1) };
     auto great_circle_haversine { grtcrcl_dist_haversine_nm(P0, P1) };
     auto great_circle_bearing { grtcrcl_init_brng(P0, P1) };
-    CoordinatesNS::CoordinatesDMSF great_circle_midpoint { grtcrcl_midpoint(P0, P1) };
-    CoordinatesNS::CoordinatesDMSF great_circle_frac { grtcrcl_frac_point(P0, P1, 0.50, great_circle) };
+    CoordinatesNS::Float::CoordinatesDMSF great_circle_midpoint { grtcrcl_midpoint(P0, P1) };
+    CoordinatesNS::Float::CoordinatesDMSF great_circle_frac { grtcrcl_frac_point(P0, P1, 0.50, great_circle) };
     Nav_vec_tF CP0_P1 { great_circle, great_circle_bearing };
-    CoordinatesNS::CoordinatesDMSF calc_P1 { grtcrcl_start_vec(P0, CP0_P1) };
+    CoordinatesNS::Float::CoordinatesDMSF calc_P1 { grtcrcl_start_vec(P0, CP0_P1) };
     auto rect_linear { rectl_dist_nm(P0, P1) };
     auto rhumb_line { rhumb_dist_nm(P0, P1) };
     auto rhumb_brg { rhumb_brng(P0, P1) };
-    CoordinatesNS::CoordinatesDMSF rhumb_mp { rhumb_midpoint(P0, P1) };
+    CoordinatesNS::Float::CoordinatesDMSF rhumb_mp { rhumb_midpoint(P0, P1) };
     Nav_vec_tF RP0_P1 { rhumb_line, rhumb_brg };
-    CoordinatesNS::CoordinatesDMSF rhumb_calc_P1 { rhumb_start_vec(P0, RP0_P1) };
-    CoordinatesNS::CoordinatesDMSF FP0 { P0 };
-    CoordinatesNS::CoordinatesDMSF FP1 { P1 };
+    CoordinatesNS::Float::CoordinatesDMSF rhumb_calc_P1 { rhumb_start_vec(P0, RP0_P1) };
+    CoordinatesNS::Float::CoordinatesDMSF FP0 { P0 };
+    CoordinatesNS::Float::CoordinatesDMSF FP1 { P1 };
 
     std::cout << name0 << " ("sv << P0 << ") "sv << name1 << " ("sv << P1 << ")"sv << std::endl;
     std::cout << "Great Circle "sv << great_circle << "nm  Small Great Circle "sv << great_circle_sm << "nm Great Circle Haversine "sv << great_circle_haversine << "nm"sv << std::endl;
--- nav_types.h
+++ nav_types.h
@@ -10,7 +10,7 @@
 
 using namespace std::literals::string_literals;
 
-namespace CoordinatesNS {
+namespace CoordinatesNS::Float {
 	using D_tF = D_t<float_t>;
 	using DM_tF = DM_t<float_t>;
 	using DMS_tF = DMS_t<float_t>;
@@ -55,6 +55,55 @@ namespace CoordinatesNS {
 	D_tF operator""_WD(long double degs_)
 	{
 		D_tF ret(static_cast<float>(-degs_));
+		return ret;
+	}
+}
+
+namespace CoordinatesNS::Double {
+	using D_tD = D_t<double>;
+	using DM_tD = DM_t<double>;
+	using DMS_tD = DMS_t<double>;
+	using Nav_vec_tD = Nav_vec_t<double>;
+	using Cart_vec_tD = Cart_vec_t<double>;
+
+	using CoordinatesDD = Coordinates<D_tD>;
+	using CoordinatesDMD = Coordinates<DM_tD>;
+	using CoordinatesDMSD = Coordinates<DMS_tD>;
+
+	D_tD operator""_D(long double degs_);
+
+	D_tD operator""_ND(long double degs_);
+	D_tD operator""_SD(long double degs_);
+	D_tD operator""_ED(long double degs_);
+	D_tD operator""_WD(long double degs_);
+
+	D_tD operator""_D(long double degs_)
+	{
+		D_tD ret(static_cast<double>(degs_));
+		return ret;
+	}
+
+	D_tD operator""_ND(long double degs_)
+	{
+		D_tD ret(static_cast<double>(degs_));
+		return ret;
+	}
+
+	D_tD operator""_SD(long double degs_)
+	{
+		D_tD ret(static_cast<double>(-degs_));
+		return ret;
+	}
+
+	D_tD operator""_ED(long double degs_)
+	{
+		D_tD ret(static_cast<double>(degs_));
+		return ret;
+	}
+
+	D_tD operator""_WD(long double degs_)
+	{
+		D_tD ret(static_cast<double>(-degs_));
 		return ret;
 	}
 }
